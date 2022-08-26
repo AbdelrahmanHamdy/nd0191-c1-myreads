3c7c72fb08f3a4c4f4aa146f805d7f0799bc196b
Abdelrhman Hamdy <abdelrhman_hamdy2013@hotmail.com>
2022-08-26 23:41:45 +0200

    BookCard Implementation 🙈

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,4 +1,6 @@
-const BookCard = () => {
+import ShelfTypes from "../shared/enums/ShelfTypes.enum";
+
+const BookCard = ({bookCard}) => {
     return (
         <li>
         <div className="book">
@@ -8,26 +10,26 @@ const BookCard = () => {
                     style={{
                         width: 128,
                         height: 193,
-                        backgroundImage:
-                            'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+                        backgroundImage:`url(${bookCard?.imageLinks?.thumbnail})`,
                     }}
                 ></div>
                 <div className="book-shelf-changer">
-                    <select>
-                        <option value="none" disabled>
+                    <select value={bookCard.shelf || ShelfTypes.NONE} onChange={() => {}}>
+                        <option value={null} disabled>
                             Move to...
                         </option>
-                        <option value="currentlyReading">
-                            Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
+                        <option value={ShelfTypes.CURRENTLY_READING}>Currently Reading</option>
+                        <option value={ShelfTypes.WANT_TO_READ}>Want to Read</option>
+                        <option value={ShelfTypes.READ}>Read</option>
+                        <option value={ShelfTypes.NONE}>None</option>
                     </select>
                 </div>
             </div>
-            <div className="book-title">To Kill a Mockingbird</div>
-            <div className="book-authors">Harper Lee</div>
+
+            <div className="book-title">{bookCard.title}</div>
+
+            {bookCard.authors && bookCard.authors.length > 0 && bookCard.authors.map ((author, i) => <div key={"author"+i} className="book-authors">{author}</div> )}
+            
         </div>
     </li>
     );
