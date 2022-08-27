2356c8098d4ab6fad87212c1b07f7002090a51ae
Abdelrhman Hamdy <abdelrhman_hamdy2013@hotmail.com>
2022-08-27 23:48:51 +0200

    add onShelfChange handeler 🥳

--- src/components/BooksShelf.js
+++ src/components/BooksShelf.js
@@ -1,13 +1,13 @@
 import BookCard from "./BookCard";
 
-const BooksShelf = ({ shelfTitle, shelf }) => {
+const BooksShelf = ({ shelfTitle, shelf, emitShelfChange }) => {
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{shelfTitle}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
 
-                    {shelf.map((book) => <BookCard key={book.id} bookCard={book} onShelfChange={() => {}}/>)}
+                    {shelf.map((book) => <BookCard key={book.id} bookCard={book} onShelfChange={(bookId, newShelf) => {emitShelfChange(bookId, newShelf)}}/>)}
 
                 </ol>
             </div>
