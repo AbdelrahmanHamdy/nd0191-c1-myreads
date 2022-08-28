60491d069f780d98969ad4a5c0e0e050579ac410
Abdelrhman Hamdy <abdelrhman_hamdy2013@hotmail.com>
2022-08-29 00:36:32 +0200

    show the correct shelf in search page 🥇

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,24 +3,53 @@ import { useState, useEffect } from "react";
 
 import * as BooksAPI from "../BooksAPI";
 import BookCard from "../components/BookCard";
+import ShelfTypes from "../shared/enums/ShelfTypes.enum";
 
 const SearchPage = () => {
 
   const [searchResult, setSearchResult] = useState([]);
+  const [wantTogetYourBooks, setWantTogetYourBooks] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [myBooks, setMyBooks] = useState([]);
 
   useEffect(() => {
+    const getAllBooks = async () => {
+      if (wantTogetYourBooks) {
+        const res = await BooksAPI.getAll();
+        setMyBooks(res);
+        setWantTogetYourBooks(false);
+      }
+    };
+
+    const mapSearchResultShelfs = (result) => {
+      return result && result.length && (result.map((book) => {
+        const usedBook = myBooks.find((b) => b.id === book.id);
+        book.shelf = usedBook && usedBook.shelf ? usedBook.shelf : ShelfTypes.NONE;
+
+        return book;
+      }))
+    }
+
+    getAllBooks();
 
     const searchForBooks = async (query, maxResult) => {
       const res = await BooksAPI.search(query, maxResult);
-      setSearchResult(res);
+      const checkedResponse = Array.isArray(res) ? res : res.items;
+      const mappedSearchResult = mapSearchResultShelfs(checkedResponse);
+      setSearchResult(mappedSearchResult);
     };
 
-    if (searchQuery) {
-      searchForBooks(searchQuery, 100);
-    }
+    searchQuery ? searchForBooks(searchQuery, 100) : setSearchResult([]);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery]);
+
+  const updateMyBooksShelf = (updatedBook, newShelf) => {
+    myBooks.find((b) => b.id === updatedBook.id) ?
+      myBooks.find((b) => b.id === updatedBook.id).shelf = newShelf :
+      myBooks.push(updatedBook);
+    setMyBooks(myBooks);
+  }
 
   return (
     <div className="search-books">
@@ -45,7 +74,7 @@ const SearchPage = () => {
 
         <ol className="books-grid">
 
-          {searchResult.length > 0 && searchResult.map((book) => <BookCard key={book.id} bookCard={book} onShelfChange={() => {}} />)}
+          {searchResult.length > 0 && searchResult.map((book) => <BookCard key={book.id} bookCard={book} onShelfChange={(updatedBook, newShelf) => { updateMyBooksShelf(updatedBook, newShelf) }} />)}
 
         </ol>
 
